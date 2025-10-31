b1c82fe6a40f03ad108f8f71f2f84d120a8f6397
M7UNDO <165899552+M7UNDO@users.noreply.github.com>
2025-11-01 00:37:39 +0200

    Semantic fixes + Back to top button

--- js/main.js
+++ js/main.js
@@ -9,14 +9,13 @@ const typographyColour = getComputedStyle(root).getPropertyValue("--typography-c
 const backgroundColour = getComputedStyle(root).getPropertyValue("--background-colour");
 
 function loadNav() {
-  const navContainer = document.getElementById("nav-placeholder");
+  const navContainer = document.querySelector("#nav-placeholder");
   if (!navContainer) return;
-  // Load nav
+
   const isGithub = window.location.hostname.includes("github.io");
   const repoName = isGithub ? "/M7UNDO" : "";
 
   const navHTML = `
-    <header>
       <nav class="nav-container">
        <div class="nav-logo">
           <div class="logo"><a href="${repoName}/index.html">M7UNDO</a></div>
@@ -31,14 +30,13 @@ function loadNav() {
         </div>
         <div class="header-actions">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
-            <a class="cart-container" href="${repoName}/cart/cart.html">
+            <a class="cart-holder" href="${repoName}/cart/cart.html">
                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>
                <span>0</span>
             </a>
             
         </div>
       </nav>
-    </header>
   `;
 
   navContainer.innerHTML = navHTML;
@@ -98,3 +96,28 @@ function updateCartCounter() {
 
   cartCounter.textContent = totalItems;
 }
+
+const backToTopBtn = document.querySelector(".back-to-top");
+
+
+//Back to top button logic
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+backToTopBtn.addEventListener("click", ()=>{
+  topFunction();
+})
